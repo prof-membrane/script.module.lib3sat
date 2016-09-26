c32d44b74de7170bace85e7458c0a568f1c00619
prof-membrane <pr0lckr@googlemail.com>
2016-09-26 20:28:02 +0200

    switched to new libmediathek interface

--- lib/lib3sat.py
+++ lib/lib3sat.py
@@ -15,7 +15,7 @@ translation = xbmcaddon.Addon(id='script.module.libMediathek').getLocalizedStrin
 
 baseXml = 'http://www.3sat.de/mediathek'
 
-
+	
 def lib3satListMain():
 	list = []
 	list.append({'name':translation(31030), 'mode':'lib3satListNew'})
@@ -40,6 +40,7 @@ def lib3satListShows():
 	letter = params['name'].replace('#','0%2D9')
 	libMediathek.addEntries(xmlservice.getXML('http://www.3sat.de/mediathek/xmlservice/web/sendungenAbisZ?characterRangeEnd='+letter+'&detailLevel=2&characterRangeStart='+letter))
 	
+
 def lib3satListDate():
 	libMediathek.populateDirDate('lib3satListDateVideos')
 def lib3satListDateVideos():
@@ -65,104 +66,12 @@ def xmlListPage():
 	libMediathek.addEntries(xmlservice.getXML(params['url']))
 	
 def xmlPlay():
-	xbmc.log('xmlplay')
 	videoUrl,subUrl,offset = xmlservice.getVideoUrl(params['url'])
 	listitem = xbmcgui.ListItem(path=videoUrl)
 	xbmcplugin.setResolvedUrl(pluginhandle, True, listitem)
 	
-
-"""	
-def lib3satListShows():
-	letter = params['name'].replace('#','0%2D9')
-	libMediathek.addEntries(xmlservice.getXML('http://www.3sat.de/mediathek/xmlservice/web/sendungenAbisZ?characterRangeEnd='+letter+'&detailLevel=2&characterRangeStart='+letter))
-	import lib3satAZ
-	letters2shows = {
-	'#':lib3satAZ.number,
-	'a':lib3satAZ.a,
-	'b':lib3satAZ.b,
-	'c':lib3satAZ.c,
-	'f':lib3satAZ.f,
-	'h':lib3satAZ.h,
-	'k':lib3satAZ.k,
-	'm':lib3satAZ.m,
-	'n':lib3satAZ.n,
-	's':lib3satAZ.s,
-	't':lib3satAZ.t,
-	'v':lib3satAZ.v,
-	'w':lib3satAZ.w,
-	}
-	#xbmc.log(str(letters2shows.get(params['name'].lower(),[])))
-	libMediathek.addEntries(letters2shows.get(params['name'].lower(),[]))
-"""
-
-"""	
-def lib3satListVideos():
-	show = params['url'].split('?red=')[-1]
-	url = 'http://www.3sat.de/mediaplayer/rss/mediathek_' + show + '.xml'
-	libMediathek.addEntries(parseRss(url))
-"""
-	
-
-"""
-def lib3satSearch():
-	keyboard = xbmc.Keyboard('', translation(31039))
-	keyboard.doModal()
-	if keyboard.isConfirmed() and keyboard.getText():
-		search_string = keyboard.getText()
-		lib3satListSearch(search_string)
-"""
-"""
-def lib3satListSearch(searchString=False):
-	if not searchString:
-		searchString = params['searchString']
-	libMediathek.addEntries(search(searchString))
-"""
-"""
-def lib3satPlay():
-	import libZdf
-	libZdf.libZdfPlay(params)
-"""
-"""
-def parseRss(url):
-	return lib3satRssParser.parseVideos(url)
-"""
-"""
-def play(dict):
-	url = getVideoUrl(dict["url"])
-	#listitem = xbmcgui.ListItem(label=video["name"],thumbnailImage=video["thumb"],path=url)
-	listitem = xbmcgui.ListItem(label=dict["name"],path=url)
-	xbmc.Player().play(url, listitem)	
-"""
-"""
-def lib3satPvrPlay(dict):
-	import lib3satPvr
-	lib3satPvr.play(dict)
-"""	
 def list():	
-	modes = {
-	'lib3satListNew': lib3satListNew,
-	'lib3satListMV': lib3satListMV,
-	'xmlListPage': xmlListPage,
-	'xmlPlay': xmlPlay,
-	'lib3satListRubrics': lib3satListRubrics,
-	'lib3satListTopics': lib3satListTopics,
-	'lib3satListLetters': lib3satListLetters,
-	'lib3satListShows': lib3satListShows,
-	'lib3satListDate': lib3satListDate,
-	'lib3satListDateVideos': lib3satListDateVideos,
-	'lib3satSearch': lib3satSearch,
 	
-	#'lib3satListVideos': lib3satListVideos,
-	#'lib3satListSearch': lib3satListSearch,
-	#'lib3satPlay': lib3satPlay
-	}
-	views = {
-	'lib3satListShows': 'shows',
-	'lib3satListVideos': 'videos',
-	'lib3satListDate': 'videos',
-	'lib3satListDateVideos': 'videos',
-	'lib3satListSearch': 'videos'
-	}
 	global params
 	params = libMediathek.get_params()
 	global pluginhandle
@@ -174,3 +83,25 @@ def list():
 		modes.get(params['mode'],lib3satListMain)()
 		libMediathek.setView(views.get(params['mode'],'default'))
 	xbmcplugin.endOfDirectory(int(sys.argv[1]),cacheToDisc=True)	
+
+modes = {
+	'lib3satListNew': lib3satListNew,
+	'lib3satListMV': lib3satListMV,
+	'xmlListPage': xmlListPage,
+	'xmlPlay': xmlPlay,
+	'lib3satListRubrics': lib3satListRubrics,
+	'lib3satListTopics': lib3satListTopics,
+	'lib3satListLetters': lib3satListLetters,
+	'lib3satListShows': lib3satListShows,
+	'lib3satListDate': lib3satListDate,
+	'lib3satListDateVideos': lib3satListDateVideos,
+	'lib3satSearch': lib3satSearch,
+	}
+views = {
+	'lib3satListShows': 'shows',
+	'lib3satListVideos': 'videos',
+	'lib3satListDate': 'videos',
+	'lib3satListDateVideos': 'videos',
+	'lib3satListSearch': 'videos'
+	}
+	
